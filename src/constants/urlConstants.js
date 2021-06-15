7d74071d23c4c7a384643ea4a027d5859f5648fd
Alfonzo <alfonzolouw9@gmail.com>
2021-06-15 12:28:46 +0200

    Updates

--- src/constants/urlConstants.js
+++ src/constants/urlConstants.js
@@ -1,3 +1,3 @@
-export const HOME_URL = '/';
-export const UX_UI_PROJECTS_URL = '/ux-ui-projects';
-export const PROJECT_URL = '/project';
+export const HOME_URL = '/ARL';
+export const UX_UI_PROJECTS_URL = '/ARL/ux-ui-projects';
+export const PROJECT_URL = '/ARL/project';
