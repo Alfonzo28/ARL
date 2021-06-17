234037c0c1f9d98e71872f3f10ad3cdc03f35149
johnojo97 <jojo@retrorabbit.co.za>
2021-06-17 18:28:39 +0200

    Catering for mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,32 @@ import PropTypes from 'prop-types';
 import { HOME_URL, UX_UI_PROJECTS_URL } from '../../constants/urlConstants';
 import './Header.scss';
 import { HOME, UX_UI_PROJECTS, TESTIMONIES, CONTACT_ME, SCROLL_ELEMENT } from '../../constants/headerConstants';
+import MobileNavDropdown from '../../assets/mobile-nav-dropdown.svg';
+import MobileCloseDropdown from '../../assets/mobile-close-dropdown.svg';
 
 class Header extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			isDesktop: false,
+			dropdownOpen: false
+		};
+	
+		this.updatePredicate = this.updatePredicate.bind(this);
+	}
+
+	componentDidMount() {
+		this.updatePredicate();
+		window.addEventListener("resize", this.updatePredicate);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.updatePredicate);
+	}
+
+	updatePredicate() {
+		this.setState({ isDesktop: window.innerWidth > 660 });
+		window.innerWidth > 660 && this.setState({ dropdownOpen: false });
 	}
 
 	renderHeaderButton(headerButton) {
@@ -80,19 +102,58 @@ class Header extends Component {
 		}
 	}
 
-	render() {        
+	handleMobileDropdown() {
+		this.setState({ dropdownOpen: !this.state.dropdownOpen });
+	}
+
+	renderMobileHeaderButtons() {
+		const headerButtons = [
+			HOME,
+			UX_UI_PROJECTS,
+			TESTIMONIES,
+			CONTACT_ME
+		];
+		return (
+			headerButtons.map(headerButton=>this.renderMobileHeaderButton(headerButton))
+		);
+	}
+
+	renderMobileHeaderButton(headerButton) {
+		return (
+			<div 
+				key={headerButton}
+				className="header-text mobile-header-button"
+				onClick={() => this.handleClick(headerButton)}
+			>
+				<div className="header-item-text">{headerButton}</div>
+				<div className="header-underline">
+				</div>
+			</div>
+		);
+	}
+
+	render() {
+		const isDesktop = this.state.isDesktop;
+
 		return (
 			<div className="header">
 				<div className="header-text header-title" onClick={() => this.handleClick(HOME)}>{'ARL'}</div>
 				<div className="f-1"/>
-				{this.renderHeaderButtons()}
+				{(!isDesktop && !this.state.dropdownOpen) && <img className="dropdown" src={MobileNavDropdown} alt="dropdown" onClick={() => this.handleMobileDropdown()}/>}
+				{isDesktop && this.renderHeaderButtons()}
+				{(!isDesktop && this.state.dropdownOpen) && 
+				<div className="mobile-header-buttons">					
+					{(!isDesktop && this.state.dropdownOpen) && <img className="dropdown" src={MobileCloseDropdown} alt="dropdown" onClick={() => this.handleMobileDropdown()}/>}
+					{this.renderMobileHeaderButtons()}
+				</div>
+				}
 				
-				<button
+				{isDesktop && <button
 					className="contact-button"
 					onClick={() => this.handleClick(CONTACT_ME)}
 				>
 					{CONTACT_ME}
-				</button>
+				</button>}
 			</div>
 		);
 	}
