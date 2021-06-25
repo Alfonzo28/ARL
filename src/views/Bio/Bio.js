c31a6c31de6127ab844efdae85d7957b2ad32727
Alfonzo <alfonzolouw9@gmail.com>
2021-06-25 15:33:09 +0200

    Bio update

--- src/views/Bio/Bio.js
+++ src/views/Bio/Bio.js
@@ -13,7 +13,16 @@ export default class Bio extends Component {
         `;
 
 		const bioParagraph2 = `
-		Qualified I.T. and Multimedia Designer with a demonstrated history of working in the broadcast media industry. Skilled in Customer Service, Research, User Experience (UX), Interaction Design (IXD) and front end development. Strong arts and design professional with a Bachelor's degree focused on Multimedia design from the University of Johannesburg.
+		Qualified I.T. and Multimedia Designer 
+		with a demonstrated history of working 
+		in the broadcast media industry.
+		Skilled in Customer Service, Research, 
+		User Experience (UX), Interaction Design 
+		(IxD), and front-end development. 
+		Strong arts and design professional 
+		with a Bachelor's degree focused on
+		Multimedia design from
+		The University of Johannesburg.
         `;
 
 		return (
